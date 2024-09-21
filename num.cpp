ad9f8a2d7673985b14e07856298e491647102601
galaxy-destroyer <joshua.samson2006@gmail.com>
2024-09-22 07:25:26 +0930

    sum

---
+++ num.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+// first number
+int num1;
+cout << "Enter ur first number: " << endl;
+cin >> num1;
+
+// second number
+int num2;
+cout << "Enter second number: " << endl;
+cin >> num2;
+
+int sum = num1 + num2;
+
+cout << "sum is: " << sum << endl;
+
+return 0;
+
+}
